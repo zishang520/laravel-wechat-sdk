3ffd0db17eda702ef85ef63941ad95c5228552c3
zishang520 <zishang520@gmail.com>
2018-11-21 10:52:36 +0800

    更新支持laravel

--- src/Providers/WechatServiceProvider.php
+++ src/Providers/WechatServiceProvider.php
@@ -1,17 +1,46 @@
 <?php
 namespace luoyy\Wechat\Providers;
 
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 use luoyy\Wechat\WechatManager;
 
 class WechatServiceProvider extends ServiceProvider
 {
+
     /**
      * Indicates if loading of the provider is deferred.
      *
      * @var bool
      */
     protected $defer = true;
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setupConfig();
+    }
+
+    /**
+     * Setup the config.
+     *
+     * @return void
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/../config/wechat.php');
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('wechat.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('wechat');
+        }
+        $this->mergeConfigFrom($source, 'wechat');
+    }
 
     /**
      * Register the application services.
@@ -22,9 +51,7 @@ class WechatServiceProvider extends ServiceProvider
     {
         $this->app->singleton(WechatManager::class, function ($app) {
             // 载入配置
-            $app->configure('wechat');
-            $config = $app->make('config')->get('wechat') ?: (require __DIR__ . '/../config/wechat.php');
-            return new WechatManager($config);
+            return new WechatManager($app->make('config')->get('wechat'));
         });
     }
     /**
